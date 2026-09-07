1d1714de12c2ee290d42037cbbe4810ad9f39ae2
Resop644 <resop322@gmail.com>
2026-09-07 20:47:28 +0300

    test: cover deterministic demo scenario

--- tests/finance.test.js
+++ tests/finance.test.js
@@ -11,4 +11,7 @@ assert.deepEqual(calc(100000, 120000), { income: 100000, expense: 120000, profit
 assert.deepEqual(calc(0, 1000), { income: 0, expense: 1000, profit: -1000, margin: 0 });
 assert.deepEqual(calc(250000.5, 100000.25), { income: 250000.5, expense: 100000.25, profit: 150000.25, margin: 59.99998003995992 });
 
+// Acceptance scenario used by scripts/seed-demo.js.
+assert.deepEqual(calc(1000000, 500000), { income: 1000000, expense: 500000, profit: 500000, margin: 50 });
+
 console.log('Finance calculation tests: OK');
